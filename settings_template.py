b688e8bd15a54506906a6d468f56526bd4bec8c0
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-12-02 19:29:34 -0800

    Added settings_template.py

---
+++ settings_template.py
@@ -0,0 +1,15 @@
+# Board information
+BOARD = "ESP32"
+
+# Wifi settings
+WIFI = {
+    "SSID": "my_ssid",
+    "PASSWORD": "my_password",
+    "BLINK_ON_CONNECT": False,
+}
+
+# Board settings
+PINS = {
+    "LED": 2,
+    "BUTTON": 0,
+}
